ee22cc49d37f166e821fdc63ad5c08e72b180890
Manish <manish@clearstack.io>
2021-08-17 20:24:10 +0530

    Added flash msg style

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -16,7 +16,7 @@ class ArticlesController < ApplicationController
     @article = Article.new(article_params)
 
     if @article.save
-      flash[:notice] = "Article was successfully created!"
+      flash[:success] = "Article was successfully created!"
       redirect_to article_path(@article)
     else
       render :new
@@ -35,7 +35,7 @@ class ArticlesController < ApplicationController
     # @article = Article.find(params[:id])
 
     if @article.update(article_params)
-      flash[:notice] = "Article was successfully Updated!"
+      flash[:success] = "Article was successfully Updated!"
       redirect_to article_path(@article)
     else
       render :edit
@@ -45,7 +45,7 @@ class ArticlesController < ApplicationController
   def destroy
     # @article = Article.find(params[:id])
     @article.destroy
-    flash[:notice] = "Article was successfully Deleted"
+    flash[:danger] = "Article was successfully Deleted"
     redirect_to articles_path
   end
 
