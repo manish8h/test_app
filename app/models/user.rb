316c7e005e8ece835ae711fcb9ffd99b70c20f49
Manish <manish@clearstack.io>
2021-08-18 21:36:20 +0530

    Add validations for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,14 @@
 class User < ApplicationRecord
   has_many :comments
+  #don't allow user with same username up or lowercase
+  validates :username, presence: true, uniqueness: {case_sensitive: false},
+            length: {minimum: 3, maximum: 25}
+
+  #refer rubular.com for regex
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  validates :email, presence: true, uniqueness: {case_sensitive: false},
+            length: {maximum: 105}, format: { with: VALID_EMAIL_REGEX }
+
 
 end
