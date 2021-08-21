54d725129baad55ca08f2bee9e812d899ac5e267
Manish <manish@clearstack.io>
2021-08-21 23:24:48 +0530

    Add user pwd digest

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,4 +15,6 @@ class User < ApplicationRecord
 
   before_save { self.email = email.downcase }
 
+  has_secure_password
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_21_162423) do
+ActiveRecord::Schema.define(version: 2021_08_21_174121) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 2021_08_21_162423) do
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
   end
 
   add_foreign_key "comments", "users"
