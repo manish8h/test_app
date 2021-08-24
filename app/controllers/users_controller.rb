276a13f28f01daad746720ddf908ad45ac45cc77
Manish <manish@clearstack.io>
2021-08-24 21:48:53 +0530

    fix error msg issue

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,18 +23,26 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
 
-    # respond_to do |format|
+    # if @user.save
+    #   flash[:success] = "Welcome to the Test App #{@user.username}!"
+    #   redirect_to articles_path
+    # else
+    #   render :new
+    # end
+    # ===============
+    # only render or redirect_to per action
+    # ===============
+    respond_to do |format|
       if @user.save
         flash[:success] = "Welcome to the Test App #{@user.username}!"
         # format.html { redirect_to @user, notice: "User was successfully created." }
-        redirect_to articles_path
-        # format.json { render :show, status: :created, location: @user }
+        format.html { redirect_to articles_path }
+        format.json { render :'articles/index', status: :created, location: @user }
       else
-        # format.html { render :new, status: :unprocessable_entity }
-        # format.json { render json: @user.errors, status: :unprocessable_entity }
-        redirect_to signup_path
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
       end
-    # end
+    end
   end
 
   # PATCH/PUT /users/1 or /users/1.json
